c21bed79c6a885c07e1cdf46d0b09fbfbaff3f94
Cici Hou <41445818+cicihou@users.noreply.github.com>
2023-02-26 18:38:50 -0800

    mod: tiny

--- project1/router.js
+++ project1/router.js
@@ -1,11 +1,9 @@
 const express = require("express");
+
 const { guess, newGame, login, logout } = require("./controller.js");
-
 const page = require("./page.js");
-
 const router = express.Router();
 
-// PUT ROUTE HERE
 router.get("/", page.renderHome);
 
 router.post("/login", login);
--- project1/server.js
+++ project1/server.js
@@ -1,14 +1,12 @@
 "use strict";
 const express = require("express");
-// const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const webRoutes = require("./router.js");
 
 const app = express();
-const PORT = 30001;
+const PORT = 3000;
 
 app.use(express.static("./public"));
-// app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true })); // Use express.urlencoded to parse form data, use extended: true to omit warning
 
