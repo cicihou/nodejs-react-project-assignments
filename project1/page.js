d8674143347aeb15180c80723aba086f27462250
Cici Hou <41445818+cicihou@users.noreply.github.com>
2023-02-26 18:26:53 -0800

    add: html

--- project1/page.js
+++ project1/page.js
@@ -1,3 +1,4 @@
+const controller = require("./controller.js");
 const info = require("./userInfo.js")
 const words = require("./words")
 
@@ -18,10 +19,8 @@ const page = {
       </head>
     
       <body>
-          <div class="container">
-            ${page.getNavElement(username)}
-            ${page.getMainElement(username)}
-          </div>
+          ${page.getNavElement(username)}
+          ${page.getMainElement(username)}
       </body>
     
     </html>
@@ -37,16 +36,16 @@ const page = {
     getNavElement(username) {
         return `
       <nav class="nav">
-        <ul class="nav-list">
-            ${
+        ${
             !username
-                ? '<li><a href="/login">Log In</a></li>'
-                : `<li>
-                    <form action="/logout" method="post"><button type="submit">Log Out</button>
-                    </form>
-                  </li>`
+                ? `<header class="page-header"><h2>Welcome!</h2><p>Please login.</p></header>`
+                : `<header class="page-header">
+                  <h2>Welcome! ${username}</h2>
+                  <p>
+                      <form action="/logout" method="post"><button type="submit" class="submit-button"/>Log out</form>
+                  </p>
+              </header>`
         }
-        </ul>
       </nav>
     `;
     },
@@ -61,90 +60,69 @@ const page = {
         let guessCountElement = "";
         let wonMessageElement = "";
         let guessFormElement = "";
-        let secretWordsElemet = `<p class="game-prompt">Secret word list: ${words
-            .filter((entry) => !userStat.guessedWord.includes(entry)) // Filter out guessed words
-            .join(", ")}</p>`;
         if (userStat && Object.keys(userStat).length) {
             if (userStat.guessedWord.length) {
-                lastGuessElement = `<p class="game-prompt">
-                              Last guess "${
-                    userStat.guessedWord[
-                    userStat.guessedWord.length - 1
-                        ][0]
-                }" was 
-                              ${userStat.lastGuessStatus}, and have 
-                              ${
-                    userStat.guessedWord[
-                    userStat.guessedWord.length - 1
-                        ][1]
-                }
-                              characters in common.
-                            </p>`;
+                lastGuessElement = `<p class="game-operation-tip">Last guess 
+                    "${userStat.guessedWord[userStat.guessedWord.length - 1][0]}" was 
+                     ${userStat.lastGuessStatus}, and have 
+                     ${userStat.guessedWord[userStat.guessedWord.length - 1][1]} characters in common.
+                </p>`;
             }
-            guessHistoryElement = `<div class="game-prompt">
-                        <p>Your guest history: </p>
-                          <ul>
-                          ${userStat.guessedWord
-                .map(
-                    (entry) =>
-                        `<li>${entry.join(
-                            ": "
-                        )} characters in common</li>`
-                )
-                .join("")}
-                          </ul>
-                      </div>`;
-            guessCountElement = `<p class="game-prompt">
-                            You have ${userStat.guessedCount} guess.
-                           </p>`;
+            guessHistoryElement = `<div class="game-operation-list"><ul>
+                ${userStat.guessedWord.map((entry) =>`<li>${entry.join(": ")} characters in common</li>`).join("")}
+                </ul></div>`;
+            guessCountElement = `<p class="game-prompt">You have ${userStat.guessedCount} guess.</p>`;
             guessFormElement = `<form action="/guess" method="post" class="new-guess">
                             <label>Guess a new word: <input type="text" name="guess" placeholder="word" required></label>
-                            <button type="submit">Submit</button>
-                          </form>`;
+                            <button type="submit" class="submit-button">Submit</button></form>`;
         }
         if (userStat.lastGuessStatus === 'correct') {
             wonMessageElement = `<p class="game-prompt win">You won the game!</p>`;
         }
         return `
-      <main class="main">
-          <div class="welcome-prompt">
-              <h1 class="welcome-prompt-text">Welcome! <span class="username-text">${username}</span></h1>
-          </div>
-              
-                  <div class="game">
-                      ${page.getErrorMsg(username)}
-                      ${secretWordsElemet}
-                      ${lastGuessElement}
-                      ${guessHistoryElement}
-                      ${guessCountElement}
-                      ${wonMessageElement}
-                      ${guessFormElement}
-                  </div>
-              <form action="/new-game" method="post" class="new-game">
-                  <button type="submit">New Game</button>
-              </form>
-          </div>
-      </main>
+            <main class="page-content">
+                <div class="home-panel">
+                    <p class="prompt">Secret word list: </p>
+                    ${page.getErrorMsg(username)}
+                    <p class="words-list">
+                        ${words.filter((entry) => !userStat.guessedWord.includes(entry)).join(", ")}
+                    </p>
+                    <p class="prompt">Your operation: </p>
+                    ${lastGuessElement}
+                    <div class="game-line">
+                        <p>Your guest history: </p>
+                        ${guessFormElement}
+                    </div>
+                    <div class="words-list">
+                        ${guessHistoryElement}
+                    </div>
+                    <div class="game-line">
+                        ${guessCountElement}
+                        ${wonMessageElement}
+                    </div>
+                    <form action="/new-game" method="post" class="new-game">
+                        <button type="submit" class="submit-button">New Game</button>
+                    </form>
+                </div>
+            </main>
     `;
     },
 
     getLoginPage(err) {
         return `
-    <main class="main">
-      <div class="welcome-prompt">
-          <h1 class="welcome-prompt-text">Welcome!</h1>
-      </div>
-      <div class="login">
-          <p class="login-error-msg">${
-            err || "Invalid Credential, please login again: "
-        }</p>
-          <form action="/login" method="POST" class="login-form">
-              <label>Username<input type="text" name="username" required></label>
-              <button type="submit">Log In</button>
-          </form>
-      </div>
-    </main>     
-    `;
+            <main class="page-content">
+                <div class="login-panel">
+                    <p class="prompt">Please Log in: </p>
+                    <form class="login-form" action="/login" method="post">
+                    <label>
+                        Username:
+                        <input class="info-input" type="text" name="username" required>
+                    </label>
+                    <button class="submit-button" type="submit">Submit</button>
+                    </form>
+                </div>
+            </main>
+        `;
     },
 
     getErrorMsg(username) {
