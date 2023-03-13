ab1e67a65129993bfb3e3e478c72c1200054e1c3
Cici Hou <41445818+cicihou@users.noreply.github.com>
2023-03-12 19:53:19 -0700

    add: a8

---
+++ work/08-js-service-calls/webpack.config.js
@@ -0,0 +1,37 @@
+
+const path = require('path');
+const isProduction = false;
+
+
+const config = {
+    entry: './src/index.js',
+    devtool: 'source-map',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+                type: 'asset',
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: { presets: ["@babel/preset-env"] },
+                },
+            },
+        ],
+    },
+};
+
+module.exports = () => {
+    if (isProduction) {
+        config.mode = 'production';
+    } else {
+        config.mode = 'development';
+    }
+    return config;
+};
